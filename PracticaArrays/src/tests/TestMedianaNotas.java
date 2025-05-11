3b0fc8422e0782609d8208cdc82a139acc2a6ffa
agn0026 <agn0026@alu.medac.es>
2025-05-11 20:45:18 +0200

    3 commit

--- PracticaArrays/src/tests/TestMedianaNotas.java
+++ PracticaArrays/src/tests/TestMedianaNotas.java
@@ -2,13 +2,35 @@ package tests;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import util.MisArrays;
 
 class TestMedianaNotas {
 
+static float arrayNotas [];
+	
+	@BeforeAll
+	static void setup() {
+		arrayNotas = new float [] {4.5f, 7.2f, 10f, 1.2f, 2f};
+	}
+
 	@Test
 	void testMedianaNotas() {
-		fail("Not yet implemented");
+		float numObtenida = Math.round(MisArrays.medianaNotas(arrayNotas)*100)/100;
+		float numEsperada = Math.round(4.5f*100)/100;
+		assertEquals(numEsperada, numObtenida);
 	}
+	
+	@Test
+	void testException() {
+		float arrayNotas2 [] = new float [] {4.5f, 11f, -2f, 1.2f, 2f};
+		Exception exception = assertThrows(IllegalArgumentException.class, () -> MisArrays.medianaNotas(arrayNotas2));
+		String mensajeEsperado = "Alguno de los numeros del array no esta entre 0 y 10";
+		String mensajeObtenido = exception.getMessage();
+		assertEquals(mensajeEsperado, mensajeObtenido);
+	}
+
 
 }
--- PracticaArrays/src/util/MisArrays.java
+++ PracticaArrays/src/util/MisArrays.java
@@ -1,6 +1,5 @@
 package util;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 
 /**
@@ -32,5 +31,39 @@ public abstract class MisArrays {
 		float media = suma/nuevoArray.length;
 		return media;
 	}
+	
+	/**
+	 * Calcula la mediana de notas que se encuentran en un array pasado por parametro y su valores comprenden entre el 0 y 10 con decimales
+	 *
+	 * @param nuevoArray
+	 *
+	 * @return la mediana con decimales de las notas pasadas
+	 *
+	 * @throws IllegalArgumentException
+	 */
+	public static float medianaNotas (float nuevoArray []) throws IllegalArgumentException{
+		for (int i=0; i<nuevoArray.length; i++) {
+			if (nuevoArray[i]<0 || nuevoArray[i]>10) {
+				throw new IllegalArgumentException ("Alguno de los numeros del array no esta entre 0 y 10");
+			}
+		}
+		
+		Arrays.sort(nuevoArray);   //importar clase para funcionamiento
+		
+		float media;
+		if (nuevoArray.length%2 == 0) {
+			int posicion1= (nuevoArray.length/2) -1;
+			int posicion2 = posicion1 + 1;
+			
+			float valor1 = nuevoArray[posicion1];
+			float valor2 = nuevoArray[posicion2];
+			
+			media = (valor1+valor2)/2;
+		}else {
+			media = nuevoArray[(nuevoArray.length/2)];
+		}
+		return  media;
+	}
+
 	
 }
